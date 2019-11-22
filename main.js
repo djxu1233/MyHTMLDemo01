60db305938e32e45747f09245313818117d3aae4
djxu <djxu1233@163.com>
2019-11-22 16:00:19 +0800

    修改main.js

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ select.addEventListener('change', setWeather);
 
 function setWeather() {
     let choice = select.value;
+
+    /*
     if (choice === 'sunny') {
         para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
     } else if (choice === 'rainy') {
@@ -15,5 +17,32 @@ function setWeather() {
         para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
     } else {
         para.textContent = '';
+    }
+    */
+
+    switch (choice) {
+        case 'sunny':
+            para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
+            break;
+        case 'rainy': 
+            para.textContent = 'Rain is falling outside; take a rain coat and a brolly, and don\'t stay out for too long.';
+            break;
+        case 'snowing':
+            para.textContent =  'The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
+            break;
+        case 'overcast':
+            para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
+            break;
+    
+        default:
+            para.textContent = '';
+            break;
+    }
+
+    for (const key in object) {
+        if (object.hasOwnProperty(key)) {
+            const element = object[key];
+            
+        }
     }
 }
